a360da25e7e9adc7913319b281e93b522a1e28fb
caliz83 <lizg1308@gmail.com>
2023-08-08 20:24:49 -0700

    first commit

---
+++ ReminderAppLecture/src/App.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import ReminderList from './Components/ReminderList'
+import Reminder from './Models/Reminders'
+import ReminderServices from './Services/Reminder'
+import NewReminder from './Components/NewReminder'
+
+// const reminders: Reminder[] = [
+//   {id: 1, title: 'Reminder1'}
+// ]
+//this got copied into the end of the useState statement, delete the rest
+
+function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([])
+
+  useEffect(() => {
+    loadReminders();  
+  }, [])
+
+  const loadReminders = async () => {
+    const reminders = await ReminderServices.getReminders();
+    setReminders(reminders)
+  }
+
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter(reminder => reminder.id !== id))
+  }
+
+  const addReminder = async (title: string) => {
+    const newReminder = await ReminderServices.addReminders(title);
+    setReminders([newReminder, ...reminders]);
+  }
+
+  return (
+    <div className="App">
+      <NewReminder onAddReminder={addReminder} />
+      <ReminderList items={reminders} onRemoveReminder={(removeReminder)} />   
+    </div>
+  )
+}
+
+export default App
---
+++ ReminderAppLecture/src/Components/ReminderList.tsx
@@ -0,0 +1,22 @@
+import Reminder from "../Models/Reminders";
+
+interface ReminderListProps {
+  items: Reminder[]; //interface Reminder to set this one up; moved to Reminders.ts
+  onRemoveReminder: (id: number) => void
+}
+
+//instead of ..... = (props: ReminderListProps)
+const ReminderList = ({ items, onRemoveReminder }: ReminderListProps) => {
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li className="list-group-item" key={item.id}>
+          {item.title}
+          <button className="btn btn-outline-danger rounded-pill mx-5" onClick={()=> onRemoveReminder(item.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ReminderList;
